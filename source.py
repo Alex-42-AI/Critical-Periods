74e10e9f1a1a3aae875ec7e8fe498f9423548898
Alex-42-AI <81178614+Alex-42-AI@users.noreply.github.com>
2026-08-08 00:53:23 +0300

    Refactor quantization logic and model setup
    Refactor quantization functions and update model handling. Adjusted variable names for clarity and improved experiment setup.

--- source.py
+++ source.py
@@ -10,6 +10,8 @@ from pandas import DataFrame
 
 from pathlib import Path
 
+from json import dump
+
 
 def quantize_tensor(weight):
     max_int = 2 ** (q_bits - 1) - 1
@@ -18,160 +20,229 @@ def quantize_tensor(weight):
     return torch.round(weight / scale) * scale
 
 
-def quantize(layer):
+def quantize(lyr):
     with torch.no_grad():
-        for module in layer.modules():
+        for module in lyr.modules():
             if isinstance(module, torch.nn.Linear):
                 weight = module.weight
                 quantized = quantize_tensor(weight)
                 weight.copy_(quantized)
 
 
+# def last_layer_hook(module, inp, out):
+#     captures["last_layer"] = out[0].detach()
+#
+#
+# def norm_hook(module, inp, out):
+#     captures["norm"] = out.detach()
+
+
 device = "cuda" if (p := torch.cuda.is_available()) else "cpu"
 
-original_type = (torch.float16, torch.float32, torch.float64)[0]
+original_types = (torch.float16, torch.float32, torch.float64)
 
-q_bits = (2, 4, 8, 16, 32)[2]
+q_bits_ls = (32, 16, 8, 4, 2)
 
-model_name = ("HuggingFaceTB/SmolLM2-360M", "HuggingFaceTB/SmolLM2-1.7B-Instruct", "HuggingFaceTB/SmolLM3-3B",
-              "Qwen/Qwen2.5-3B", "Qwen/Qwen2.5-7B-Instruct", "microsoft/Phi-3-mini-4k-instruct",
-              "mistralai/Mistral-7B-Instruct-v0.3")[0]
+model_names = ("HuggingFaceTB/SmolLM2-360M", "HuggingFaceTB/SmolLM2-1.7B-Instruct", "HuggingFaceTB/SmolLM3-3B",
+               "Qwen/Qwen2.5-3B", "Qwen/Qwen2.5-7B-Instruct", "microsoft/Phi-3-mini-4k-instruct",
+               "mistralai/Mistral-7B-Instruct-v0.3")
 
 prompts = ["Explain gravity.", "What is 173 × 29?", "Write a Python function to reverse a list.",
            "Translate 'Good morning' into Bulgarian.", "Why is the sky blue?"]
 
 case = 0
+experiments = []
 
-case_dir = Path(f"results/case{case:03d}")
-case_dir.mkdir(parents=True)
+for model_name in model_names:
+    for i, original_type in enumerate(original_types):
+        for q_bits in q_bits_ls[2 - i:]:
+            experiments.append((model_name, original_type, q_bits))
 
-metadata_file = case_dir / "metadata.txt"
+for model_name, original_type, q_bits in experiments:
+    print(model_name, original_type, q_bits)
 
-prompts_dir = case_dir / "prompts"
-prompts_dir.mkdir(parents=True, exist_ok=True)
+    case_dir = Path(f"results/case{case:02d}")
+    case_dir.mkdir(parents=True)
+    case += 1
 
-with open(metadata_file, "w") as f:
-    f.write(f"Device: {device}\nModel: {model_name}\nOriginal type: {original_type}\nQuantization: int{q_bits}\n")
+    metadata_file = case_dir / "metadata.txt"
 
-global_heatmap_mse = []
+    prompts_dir = case_dir / "prompts"
+    prompts_dir.mkdir(parents=True, exist_ok=True)
 
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+    with open(metadata_file, "w") as f:
+        f.write(f"Device: {device}\nModel: {model_name}\nOriginal type: {original_type}\nQuantization: int{q_bits}\n")
 
-for i, prompt in enumerate(prompts):
-    prompt_dir = prompts_dir / f"prompt{i:03d}"
-    prompt_dir.mkdir(parents=True, exist_ok=True)
-    prompt_heatmap_mse = []
+    global_heatmap_mae, global_result_json, global_RMSNorm_json = [], [], []
 
-    with open(prompt_dir / "content.txt", "w", encoding="utf-8") as f:
-        f.write(f"{prompt}\n")
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    inputs = tokenizer(prompt, return_tensors="pt")
-    inputs = {k: v.to(device) for k, v in inputs.items()}
+    for i, prompt in enumerate(prompts):
+        print(prompt)
 
-    model = AutoModelForCausalLM.from_pretrained(model_name, dtype=original_type).to(device)
-    model.eval()
+        prompt_dir = prompts_dir / f"prompt{i}"
+        prompt_dir.mkdir(parents=True, exist_ok=True)
+        prompt_heatmap_mae, prompt_result_json, prompt_RMSNorm_json = [], [], []
 
-    with torch.inference_mode():
-        outputs_fp = model(**inputs, output_hidden_states=True)
-        baseline_hidden = outputs_fp.hidden_states
+        with open(prompt_dir / "content.txt", "w", encoding="utf-8") as f:
+            f.write(f"{prompt}\n")
 
-    del model, outputs_fp
+        inputs = tokenizer(prompt, return_tensors="pt")
+        inputs = {k: v.to(device) for k, v in inputs.items()}
 
-    model_q = AutoModelForCausalLM.from_pretrained(model_name, dtype=original_type).to(device)
-    model_q.eval()
+        model = AutoModelForCausalLM.from_pretrained(model_name, dtype=original_type).to(device)
+        model.eval()
 
-    for j, layer in enumerate(model_q.model.layers[:-1]):
-        result = []
-        q_layer = prompt_dir / f"q_layer{j:03d}"
-        q_layer.mkdir(exist_ok=True)
-        damage_plot = {"layer": [], "mse": [], "cosine": []}
+        # Verification experiment:
+        # hidden_states[-1] corresponds to the output of the model's final RMSNorm,
+        # rather than an additional transformer layer. This was verified by comparing
+        # the final transformer-layer and RMSNorm outputs using forward hooks.
+        # with torch.inference_mode():
+        #     outputs = model(**inputs, output_hidden_states=True)
+        #
+        # captures = {}
+        #
+        # h1 = model.model.layers[-1].register_forward_hook(last_layer_hook)
+        # h2 = model.model.norm.register_forward_hook(norm_hook)
+        #
+        # with torch.inference_mode():
+        #     outputs = model(**inputs, output_hidden_states=True)
+        #
+        # h1.remove(), h2.remove()
+        #
+        # print(torch.mean(torch.abs(captures["last_layer"] - outputs.hidden_states[-1])))
+        # print(torch.mean(torch.abs(captures["norm"] - outputs.hidden_states[-1])))
+        # print(torch.mean(torch.abs(captures["last_layer"] - captures["norm"])))
 
-        with torch.no_grad():
-            restore_layer = {k: v.clone() for k, v in layer.state_dict().items()}
+        with torch.inference_mode():
+            outputs_fp = model(**inputs, output_hidden_states=True)
+            baseline_hidden = outputs_fp.hidden_states
 
-        try:
-            quantize(layer)
+        del model, outputs_fp
 
-            with torch.inference_mode():
-                outputs_q = model_q(**inputs, output_hidden_states=True)
+        model_q = AutoModelForCausalLM.from_pretrained(model_name, dtype=original_type).to(device)
+        model_q.eval()
 
-            for k, (fp, q) in enumerate(zip(baseline_hidden, outputs_q.hidden_states)):
-                d = torch.mean((fp - q) ** 2).item()
-                cos = torch.nn.functional.cosine_similarity(fp.flatten(), q.flatten(), 0).item()
-                result.append(f"{k:02d}; MSE: {d:.4f}; cos: {cos:.4f}\n")
+        for j, layer in enumerate(model_q.model.layers[:-1]):
+            layer_result_json = []
+            q_layer = prompt_dir / f"q_layer{j:03d}"
+            q_layer.mkdir(exist_ok=True)
+            damage_plot = {"layer": [], "mae": [], "cosine": []}
 
-                damage_plot["layer"].append(k), damage_plot["mse"].append(d), damage_plot["cosine"].append(cos)
-                prompt_heatmap_mse.append({"quantized_layer": j, "measured_layer": k, "mse": d})
-                global_heatmap_mse.append({"prompt": prompt, "quantized_layer": j, "measured_layer": k, "mse": d})
-
-            with open(q_layer / "results.txt", "w") as f:
-                f.write("".join(result))
-
-        finally:
-            layer.load_state_dict(restore_layer)
-
-            del restore_layer
+            with torch.no_grad():
+                restore_layer = {k: v.clone() for k, v in layer.state_dict().items()}
 
             try:
-                del outputs_q
+                quantize(layer)
 
-            except NameError:
-                ...
+                with torch.inference_mode():
+                    outputs_q = model_q(**inputs, output_hidden_states=True)
 
-        fig, ax1 = plt.subplots(figsize=(7, 4))
+                for k, (fp, q) in enumerate(list(zip(baseline_hidden, outputs_q.hidden_states))[:-1]):
+                    mae = torch.mean(torch.abs(fp.float() - q.float())).item()
+                    cos = torch.nn.functional.cosine_similarity(fp.float().flatten(), q.float().flatten(), 0).item()
 
-        ax1.plot(damage_plot["layer"], damage_plot["mse"], marker="o", color="tab:red")
+                    damage_plot["layer"].append(k), damage_plot["mae"].append(mae), damage_plot["cosine"].append(cos)
 
-        ax1.set_xlabel("Measured layer")
-        ax1.set_ylabel("MSE", color="tab:red")
+                    prompt_heatmap_mae.append({"quantized_layer": j, "measured_layer": k, "mae": mae})
+                    global_heatmap_mae.append({"prompt": i, "quantized_layer": j, "measured_layer": k, "mae": mae})
 
-        ax2 = ax1.twinx()
+                    layer_result_json.append({"measured layer": k, "mae": mae, "cos": cos})
+                    prompt_result_json.append({"quantized layer": j, "measured layer": k, "mae": mae, "cos": cos})
+                    global_result_json.append({"prompt": prompt, "quantized layer": j, "measured layer": k, "mae": mae, "cos": cos})
 
-        ax2.plot(damage_plot["layer"], damage_plot["cosine"], marker="s", color="tab:blue")
+                with open(q_layer / f"layer{j}_RMSNorm.json", "w") as f:
+                    fp, q = baseline_hidden[-1], outputs_q.hidden_states[-1]
 
-        ax2.set_ylabel("Cosine similarity", color="tab:blue")
+                    mae = torch.mean(torch.abs(fp.float() - q.float())).item()
+                    cos = torch.nn.functional.cosine_similarity(fp.float().flatten(), q.float().flatten(), 0).item()
 
-        plt.title("Damage per layer")
+                    dump({"mae": mae, "cos": cos}, f)
+                    prompt_RMSNorm_json.append({"quantized layer": j, "mae": mae, "cos": cos})
+                    global_RMSNorm_json.append({"prompt": prompt, "quantized layer": j, "mae": mae, "cos": cos})
 
-        plt.grid(True)
+                with open(q_layer / f"layer{j}_results.json", "w") as f:
+                    dump(layer_result_json, f)
 
-        plt.savefig(q_layer / "damage_plot.png")
+                    del layer_result_json
 
-        plt.close()
+            finally:
+                layer.load_state_dict(restore_layer)
 
-        del damage_plot
+                del restore_layer, outputs_q
 
-    del model_q, baseline_hidden, inputs
+            fig, ax1 = plt.subplots(figsize=(9, 4))
 
-    collect()
+            ax1.plot(damage_plot["layer"], damage_plot["mae"], marker="o", color="tab:red")
+            ax1.set_xlabel("Measured layer")
+            ax1.set_ylabel("MAE", color="tab:red")
 
-    if p:
-        torch.cuda.empty_cache()
-        torch.cuda.synchronize()
+            ax2 = ax1.twinx()
+            ax2.plot(damage_plot["layer"], damage_plot["cosine"], marker="s", color="tab:blue")
+            ax2.set_ylabel("Cosine similarity", color="tab:blue")
 
-    df = DataFrame(prompt_heatmap_mse)
+            plt.title(f"{Path(model_name).name} | Quantized layer {j} | {str(original_type)[6:]} → int{q_bits}")
+            plt.grid(True)
 
-    pivot = df.pivot(index="quantized_layer", columns="measured_layer", values="mse")
+            plt.savefig(q_layer / f"damage_plot_layer{j}.png")
+            plt.savefig(q_layer / f"damage_plot_layer{j}.pdf")
+            plt.close()
+
+            del damage_plot
+
+        del model_q, baseline_hidden, inputs
+
+        collect()
+
+        if p:
+            torch.cuda.empty_cache()
+            torch.cuda.synchronize()
+
+        with open(prompt_dir / f"prompt{i}_results.json", "w") as f:
+            dump(prompt_result_json, f)
+
+            del prompt_result_json
+
+        with open(prompt_dir / f"prompt{i}_RMSNorm.json", "w") as f:
+            dump(prompt_RMSNorm_json, f)
+
+            del prompt_RMSNorm_json
+
+        df = DataFrame(prompt_heatmap_mae)
+
+        pivot = df.pivot(index="quantized_layer", columns="measured_layer", values="mae")
+
+        plt.figure(figsize=(10, 8))
+        plt.imshow(pivot, aspect="auto", cmap="viridis")
+        plt.title(f"{Path(model_name).name}\nPrompt {i}: MAE representation damage | {str(original_type)[6:]} → int{q_bits}")
+        plt.xticks(range(len(pivot.columns)), pivot.columns)
+        plt.yticks(range(len(pivot.index)), pivot.index)
+        plt.xlabel("Measured layer")
+        plt.ylabel("Quantized layer")
+        plt.colorbar(label="Hidden-state MAE")
+        plt.savefig(prompt_dir / f"prompt{i}_heatmap_mae.png", bbox_inches="tight")
+
+    del tokenizer
+
+    with open(case_dir / "global_results.json", "w") as f:
+        dump(global_result_json, f)
+
+        del global_result_json
+
+    with open(case_dir / "global_RMSNorm.json", "w") as f:
+        dump(global_RMSNorm_json, f)
+
+        del global_RMSNorm_json
+
+    df = DataFrame(global_heatmap_mae)
+    df = (df.groupby(["quantized_layer", "measured_layer"], as_index=False)[["mae"]].mean())
+
+    pivot = df.pivot(index="quantized_layer", columns="measured_layer", values="mae")
 
     plt.figure(figsize=(10, 8))
-    plt.imshow(pivot, aspect="auto", cmap="viridis")
-    plt.xticks(range(len(pivot.columns)))
-    plt.yticks(range(len(pivot.index)))
+    plt.imshow(pivot, aspect="auto")
+    plt.title(f"Model: {Path(model_name).name} | {str(original_type)[6:]} → int{q_bits}")
     plt.xlabel("Measured layer")
     plt.ylabel("Quantized layer")
-    plt.colorbar(label="MSE")
-    plt.savefig(prompt_dir / "heatmap_mse.png", bbox_inches="tight")
-
-del tokenizer
-
-df = DataFrame(global_heatmap_mse)
-df = (df.groupby(["quantized_layer", "measured_layer"], as_index=False)[["mse"]].mean())
-
-pivot = df.pivot(index="quantized_layer", columns="measured_layer", values="mse")
-
-plt.figure(figsize=(10, 8))
-plt.imshow(pivot, aspect="auto")
-plt.xlabel("Measured layer")
-plt.ylabel("Quantized layer")
-plt.colorbar(label="MSE")
-plt.savefig(case_dir / "heatmap_mse.png", bbox_inches="tight")
+    plt.colorbar(label="Hidden-state MAE")
+    plt.savefig(case_dir / "heatmap_mae.png", bbox_inches="tight")
